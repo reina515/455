7ac8918e0c27c6d698f9a27dbefa82fe70a06a61
Ziad <ziadabillama2006@gmail.com>
2025-11-19 15:29:06 +0200

    Add MissionAttemptPage route to App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,3 +1,4 @@
+// Frontend/src/App.js
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -13,6 +14,7 @@ import SignupPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
 import CipherLabPage from "./pages/CipherLabPage";
 import MissionsPage from "./pages/MissionsPage";
+import MissionAttemptPage from "./pages/MissionAttemptPage";
 import ProfilePage from "./pages/ProfilePage";
 import OAuthCallbackPage from "./pages/OAuthCallbackPage";
 
@@ -60,6 +62,10 @@ function App() {
             <Route
               path="/missions"
               element={<ProtectedRoute element={<MissionsPage />} />}
+            />
+            <Route
+              path="/mission-attempt"
+              element={<ProtectedRoute element={<MissionAttemptPage />} />}
             />
             <Route
               path="/profile"
