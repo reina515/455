c0b2119b3a9b99bd5b3e21ddeabc21c83e397960
reina515 <rgh22@mail.aub.edu>
2025-11-19 22:36:34 +0200

    Fix API_BASE URL in signup fetch request

--- Frontend/src/pages/SignupPage.js
+++ Frontend/src/pages/SignupPage.js
@@ -44,7 +44,7 @@ const SignupPage = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch(`${API_BASE}/api/auth/signup`, {
+      const res = await fetch(`${API_BASE}api/auth/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
